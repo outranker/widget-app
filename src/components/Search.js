a5fa5e823b9a25f4723cba626e86747e0b9f07e8
Javohir <outranker222@gmail.com>
2020-10-24 22:17:59 +0900

    added useEffect for sending api to wiki

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  useEffect(async () => {
+    const searchArticle = () => {
+      axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+    };
+    searchArticle();
+  }, [term]);
   return (
     <div className="ui form">
       <div className="field">
@@ -11,6 +26,7 @@ const Search = () => {
           onChange={(e) => setTerm(e.target.value)}
           className="input"
         />
+        <p>{term}</p>
       </div>
     </div>
   );
